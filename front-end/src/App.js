6de78c02a194fb0d032be4e936029d078c096d0d
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-05-17 10:18:16 +0530

    Navbar done

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,15 @@
-import {BrowserRouter as Router,Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+//import Home from "./components/Home/Home";
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <Header />
+      {/* <Routes>
+          <Route> path="/" element={<Home/>}</Route>
+  </Routes>*/}
+    </Router>
   );
 }
 
---
+++ front-end/src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'; 
+import {ReactNavbar}from "overlay-navbar" ;
+import logo from "../../images/logo.png";
+import {FaUserAlt} from "react-icons/fa";
+
+const Header = () => {
+  return <ReactNavbar
+  
+  navColor1 = "white"
+  navColor2 = "hsl(219,48%,8%)"
+  burgerColor= "hsl(250, 100%, 75%)"
+  burgerColorHover= "hsl(250, 100%, 75%)"
+  logo = {logo}
+  logoWidth = "250px"
+  nav2justifyContent = "space-around"
+  nav3justifyContent = "space-around"
+  link1Text = "Home" link1Url = "/Home"
+  link2Text = "About" link2Url = "/About"
+  link3Text = "Projects" link3Url = "/Projects"
+  link4Text = "Contact" link4Url = "/Contact"
+  link1ColorHover = "white"
+  link1Color = "HSL(250,100%,75%)"
+  link1Size = "1.5rem"
+  link1Padding = "3vmax"
+  //profileIcon = {true}
+ profileIconElement = {FaUserAlt}
+ // profileIconColor = "HSL(250,100%,75%)"
+ // profileIconColorHover = "white"
+  />;
+     
+};
+
+export default Header;
