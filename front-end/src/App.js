6f03c31d211a582e1281c3a6adfcc04d525c4a6c
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-05-18 20:25:54 +0530

    Home page Work

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,18 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-//import Home from "./components/Home/Home";
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./components/Home/Home";
 import Header from './components/Header/Header';
 
 function App() {
   return (
+    <>
     <Router>
       <Header />
-      {/* <Routes>
-          <Route> path="/" element={<Home/>}</Route>
-  </Routes>*/}
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+      </Routes>
     </Router>
+    </>
   );
 }
 
